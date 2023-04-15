f93fd21b457d0a8a537d0f6ae48dfcb1923a99b2
paulecode <paul.ebert@code.berlin>
2023-04-15 19:34:50 +0200

    MetaData to Csv seems to work

--- converter.py
+++ converter.py
@@ -2,6 +2,7 @@
 
 # Imports
 
+import os
 import sys
 import mido
 import pandas as pd
@@ -9,40 +10,48 @@ import pandas as pd
 
 def main(args):
     for filename in args:
-        mid = mido.MidiFile(filename)
-        meta_data_list = []
-        for track_index, track in enumerate(mid.tracks):
-            absolute_time = 0
-            for message_index, message in enumerate(track):
-                absolute_time += message.time
-                if message.is_meta:
-                    meta_message = message.dict()
-                    if len(meta_message) == 2:
+        print(f"==={filename}===")
+        convert_to_csv(filename)
+        note_frame = pd.DataFrame()
+
+
+def convert_to_csv(filename):
+
+    mid = mido.MidiFile(filename)
+    filename = filename[:-4]
+    os.makedirs('output/' + filename)
+
+    meta_data_list = []
+    for track_index, track in enumerate(mid.tracks):
+        absolute_time = 0
+        for message_index, message in enumerate(track):
+            absolute_time += message.time
+            if message.is_meta:
+                meta_message = message.dict()
+                if len(meta_message) == 2:
+                    meta_data_list.append([
+                        track_index, absolute_time, str(
+                            message.type), 'EOF'
+                    ])
+                elif len(meta_message) == 3:
+                    meta_message.pop('type')
+                    meta_message.pop('time')
+                    for key, value in meta_message.items():
                         meta_data_list.append([
                             track_index, absolute_time, str(
-                                message.type), 'EOF'
+                                message.type), value
                         ])
-                    elif len(meta_message) == 3:
-                        meta_message.pop('type')
-                        meta_message.pop('time')
-                        for key, value in meta_message.items():
-                            meta_data_list.append([
-                                track_index, absolute_time, str(
-                                    message.type), value
-                            ])
-                    else:
-                        meta_message.pop('type')
-                        meta_message.pop('time')
-                        for key, value in meta_message.items():
-                            meta_data_list.append([
-                                track_index, absolute_time, f"{message.type}_{key}", value
-                            ])
-
-        for metadata in meta_data_list:
-            print(metadata)
-
-        meta_frame = pd.DataFrame(columns=['Type', 'Value', 'Time'])
-        note_frame = pd.DataFrame()
+                else:
+                    meta_message.pop('type')
+                    meta_message.pop('time')
+                    for key, value in meta_message.items():
+                        meta_data_list.append([
+                            track_index, absolute_time, f"{message.type}_{key}", value
+                        ])
+                meta_frame = pd.DataFrame(meta_data_list, columns=[
+                                          'track', 'time', 'key', 'value'])
+                meta_frame.to_csv(
+                    f"output/{filename}/{filename}.csv", index=False)
 
 
 if __name__ == "__main__":
